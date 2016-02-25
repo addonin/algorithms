ff07f78e7705aa969da1128dd6ad910ddc19283b
dimon <dmitrii.adonin@gmail.com>
2016-02-26 00:26:31 +0200

    Week 4, partial Board implementation

--- src/main/part1/week4/assignments/puzzle/Board.java
+++ src/main/part1/week4/assignments/puzzle/Board.java
@@ -8,6 +8,7 @@ public class Board {
 
     private int N;
     private int[][] blocks;
+    private int counter = 0;
 
     public Board(int[][] blocks) {
         this.N = blocks.length;
@@ -19,15 +20,44 @@ public class Board {
     }
 
     public int hamming() {
-        return 0;
+        int k = 0;
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = 0; j < N - 1; j++) {
+                if (blocks[i][j] != (j + 1) + (i * N)) {
+                    k++;
+                }
+            }
+        }
+        return k + counter;
     }
 
     public int manhattan() {
-        return 0;
+        int k = 0;
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = 0; j < N - 1; j++) {
+                int block = blocks[i][j];
+                if (block != 0 && block != (j + 1) + (i * N)) {
+                    int targetX = (block - 1) / N;
+                    int targetY = (block - 1) % N;
+                    int dx = i - targetX;
+                    int dy = j - targetY;
+                    k += Math.abs(dx) + Math.abs(dy);
+                }
+            }
+        }
+        return k + counter;
     }
 
     public boolean isGoal() {
-        return false;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (i == N - 1 && j == N - 1 && blocks[i][j] == 0) {
+                    return true;
+                } else if (blocks[i][j] != (j + 1) + (i * N)) {
+                    return false;
+                }
+            }
+        }
     }
 
     public Board twin() {
